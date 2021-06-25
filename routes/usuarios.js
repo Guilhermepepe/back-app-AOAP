e7600a92818e95ec322d69ae009611c85f061dd3
AlexManoel-dev <alex30985@fiec.edu.br>
2021-06-25 20:33:50 -0300

    Alterações do jsonwebtoken, por conta do problema na instância

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const mysql = require('../mysql').pool
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 
 router.post('/cadastro', (req, res, next) => {
     const { nome, bairro, cidade, ruanum, cep, datanasc, email, senha } = req.body
@@ -54,17 +53,8 @@ router.post('/login', (req, res, next) => {
                 }
                 if(result) {
                     req.session.email=req.body.email
-                    const token = jwt.sign({
-                        id_usuario: results[0].id_usuario,
-                        email: results[0].email
-                    }, 
-                    process.env.JWT_KEY, 
-                    {
-                        expiresIn: "1h"
-                    })
                     return res.status(200).send({ 
-                        mensagem: 'Autenticado com sucesso',
-                        token: token
+                        mensagem: 'Autenticado com sucesso'
                     })
                 }
                 return res.status(401).send({ mensagem: 'Falha na autenticação' })
