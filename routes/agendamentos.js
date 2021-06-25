833dc8f00756cc295a45f7f688939a713fab7666
AlexManoel-dev <82236308+AlexManoel-dev@users.noreply.github.com>
2021-06-25 20:52:52 -0300

    Update agendamentos.js

--- routes/agendamentos.js
+++ routes/agendamentos.js
@@ -10,25 +10,6 @@ router.post('/consulta', (req, res, next) => {
     console.log(req.body)
     const { value, valueEspec, date, valuehora} = req.body
     mysql.getConnection((err, conn) => {
-<<<<<<< HEAD
-        if (err) { return res.status(500).send({ error: error }) }
-        conn.query('SELECT nome_medico from especialidade where tipo_espec = ? and nome_hosp = ?', [valueEspec, value], (error, results) => {
-            conn.release()
-            if (error) { return res.status(500).send({ error: error }) }
-            response = {
-                mensagem: "O médico é esse",
-                nome_medico: results
-            }
-            res.status(200).send(response)
-        
-        conn.query('INSERT INTO consulta (nome_medico, nome_hosp , tipo_espec, dia_consulta, hora_consulta,email) values (?,?,?,?,?,?)', [response.nome_medico[0].nome_medico, value, valueEspec, date, valuehora,email], (error, results) => {
-            conn.release()
-            if (error) { return res.status(500).send({ error: error }) }
-            
-        })
-    })
-    })
-=======
         if (err) { return res.status(500).send({ error: error }) }        
             conn.query('SELECT nome_hosp, dia_consulta, hora_consulta from consulta where nome_hosp = ? and dia_consulta = ? and hora_consulta = ?', [value, date, valuehora], (error, results) => {
                 conn.release()
@@ -55,7 +36,6 @@ router.post('/consulta', (req, res, next) => {
                 }
             })
         })
->>>>>>> API
     }
     else{
         res.status(401).send('Não autorizado')
